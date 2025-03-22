60cafa997facc69b3bab041c6902541e5c1bd659
Loredana-Lungu <158364067+Loredana-Lungu@users.noreply.github.com>
2025-03-22 14:17:27 +0200

    'basename-modified2'

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
 
-const basename = process.env.NODE_ENV === 'production' ? '/goit-react-hw-05-movies' : '/';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
